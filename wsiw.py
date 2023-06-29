c666c44126e64f3454b97f59a7366c686189a143
Mojtaba Farzaneh <mojtabafarzaneh78@gmail.com>
2023-06-29 21:15:16 +0330

    adding stand-up comedy section

--- wsiw.py
+++ wsiw.py
@@ -1,10 +1,11 @@
 import random
 import datetime
 
-movies = ['no country for old man',
-          'silence',
-          'the tragedy of macbeth',
-          'the grit','blue velvet',
+random.seed()
+
+movies = [
+          'the grit',
+          'blue velvet',
           'lost highway',
           'mulholland drive',
           'the elephant man',
@@ -26,36 +27,80 @@ movies = ['no country for old man',
           'licorice pizza',
           'magnolia',
           'phantom thread',
-          'after sun',
-          'avatar',
-          'das boot',
-          'tar',
-          'the banshees of inisherin',
-          'the fabelmans',
-          'top gun maveric',
-          'the northman',
-          'triangle of sadness',
+          'a serious man',
+          '21',
+          'groundhog day',
+          'the hateful eight',
+          'casino',
+          'horible bosses',
+          'thirst',
+          'the handmaiden',
+          'decision to leave',
+          'in the mood for love',
+          'chungking express',
+          'fallen angels',
+          'happy together',
+          '2046',
+          'days of being wild',
+          'my bluebarry nights',
+          'incendies',
+          'the nights of cabiria',
+          'late spring',
+          'eternity and a day',
+          'alein',
+          'the third man',
+          'all that jazz',
+          'red beard',
+          'double indemnity',
+          'network',
+          'throne of blood',
+          'the celebration',
+          'rashomon',
+          'rocco and his brothers',
+          'a special day',
+          'whos afraid of virginia woolf?',
+          'the man who shot liberty valance',
+          'high and low',
+          'yi yi',
+          'a brighter summer day',
+          'ikiru',
+          'la haine',
+          'the human condition trilogy',
+          'tokyo story',
+          'woman in the dunes',
+          'mishima',
+          'mr klein',
+          'cure'
           ]
 
 this_year = [
     'after sun',
-    'avatar',
-    'das boot',
     'tar',
     'the banshees of inisherin',
     'the fabelmans',
-    'top gun maveric',
     'the northman',
-    'triangle of sadness',
+    'avatar 2', 
+    'dont worry darling',
 ]
 
+stand_up = [
+    'norms hitlers dog gossip and tricker',
+    'norms nothing especial',
+    'louis c.k back at the garden'
+]
 
 year = datetime.datetime.today().year 
 random_movie = random.choice(movies)
 random_this_year =random.choice(this_year)
+random_stand_up = random.choice(stand_up)
 
-choice = input(f"if you want to watch a random movie from {year} please enter number 1, if you wanna watch a random movie enter 2:\n")
+choice = input(f"if you want to watch a random movie from {year} please enter number 1, if you wanna watch a random movie enter 2 and if you wanna watch stand-up comedy enter 3:\n")
+
 if choice == '1':
-    print(f"so you wanna watch a movie from 2022?\nlet me check what I've for you tonight... you have to watch: {random_this_year}. ")
+    print(f"so you wanna watch a movie from {year}?\nlet me check what I've for you tonight... you have to watch: {random_this_year}. ")
+    
 elif choice == '2':
     print(f"so you have chosen... death.\njust kiddin, this is your movie sir: {random_movie}")
+
+elif choice == '3':
+    print(f"here is your special for the night sire: {random_stand_up}")
